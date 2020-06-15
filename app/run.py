e1db84130327bb012ca0edc52d5d3301cacb2f1f
Sleepingpirates <32914211+Sleepingpirates@users.noreply.github.com>
2020-06-15 19:54:43 -0400

    Add files via upload

---
+++ app/run.py
@@ -0,0 +1,20 @@
+from app import app
+
+from app import db, bcrypt
+from app.models import User
+
+DEFAULT_USER = "admin"
+DEFAULT_PASS = "admin"
+db.create_all()
+
+try:
+    hashed_password = bcrypt.generate_password_hash(DEFAULT_PASS).decode('utf-8')
+    user = User(username=DEFAULT_USER, password=hashed_password)
+    db.session.add(user)
+    db.session.commit()
+except:
+    print("Some error in setting up.")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
